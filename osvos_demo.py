c6acffaaa908949db30ab75c54da5da09a5f46b6
jbRegli <jeanbaptiste.regli@gmail.com>
2017-06-19 17:41:15 +0100

    working training on our own data

--- osvos_demo.py
+++ osvos_demo.py
@@ -9,35 +9,54 @@ Please consider citing the paper if you use this code.
 """
 import os
 import sys
-from PIL import Image
-import numpy as np
-import tensorflow as tf
-slim = tf.contrib.slim
+
+ROOT_DIR = os.environ['ROOT_DIR']
+try:
+    ROOT_DIR = os.environ['ROOT_DIR']
+except:
+    try:
+        ROOT_DIR = os.environ['PWD']
+    except:
+        assert ROOT_DIR, 'ROOT_DIR was not able to be defined'
+os.chdir(ROOT_DIR)
+sys.path.append(ROOT_DIR)
+
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from scipy.misc import imsave
+import tensorflow as tf
+
+slim = tf.contrib.slim
+
 # Import OSVOS files
 root_folder = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(root_folder))
 import osvos
 from dataset import Dataset
+
 os.chdir(root_folder)
 
 # User defined parameters
-seq_name = "car-shadow"
+seq_name = "truss_complex"
 gpu_id = 0
 train_model = True
-result_path = os.path.join('DAVIS', 'Results', 'Segmentations', '480p', 'OSVOS', seq_name)
+
+img_path = os.path.join('data', 'interim', 'osvos', 'JPEGImages', '480p', seq_name)
+anno_path = os.path.join('data', 'interim', 'osvos', 'Annotations', '480p', seq_name)
+result_path = os.path.join('data', 'processed', 'osvos', 'segmentation', '480p', seq_name)
 
 # Train parameters
 parent_path = os.path.join('models', 'OSVOS_parent', 'OSVOS_parent.ckpt-50000')
 logs_path = os.path.join('models', seq_name)
-max_training_iters = 500
+max_training_iters = 600
 
 # Define Dataset
-test_frames = sorted(os.listdir(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name)))
-test_imgs = [os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, frame) for frame in test_frames]
+test_frames = sorted(os.listdir(img_path))
+test_imgs = [os.path.join(img_path, frame) for frame in test_frames]
 if train_model:
-    train_imgs = [os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, '00000.jpg')+' '+
-                  os.path.join('DAVIS', 'Annotations', '480p', seq_name, '00000.png')]
+    train_imgs = [os.path.join(img_path, 'image-001.jpg') + ' ' +
+                  os.path.join(anno_path, 'image-001.png')]
     dataset = Dataset(train_imgs, test_imgs, './', data_aug=True)
 else:
     dataset = Dataset(None, test_imgs, './')
@@ -58,24 +77,30 @@ if train_model:
 # Test the network
 with tf.Graph().as_default():
     with tf.device('/gpu:' + str(gpu_id)):
-        checkpoint_path = os.path.join('models', seq_name, seq_name+'.ckpt-'+str(max_training_iters))
-        osvos.test(dataset, checkpoint_path, result_path)
+        checkpoint_path = os.path.join('models', seq_name, seq_name + '.ckpt-' + str(max_training_iters))
+        osvos.test(dataset, checkpoint_path, os.path.join(result_path, 'masks'))
 
-# Show results
+# Save results
 overlay_color = [255, 0, 0]
 transparency = 0.6
 plt.ion()
 for img_p in test_frames:
     frame_num = img_p.split('.')[0]
-    img = np.array(Image.open(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, img_p)))
-    mask = np.array(Image.open(os.path.join(result_path, frame_num+'.png')))
-    mask = mask/np.max(mask)
+    img = np.array(Image.open(os.path.join(img_path, img_p)))
+    mask = np.array(Image.open(os.path.join(result_path, frame_num + '.png')))
+    mask = mask / np.max(mask)
     im_over = np.ndarray(img.shape)
-    im_over[:, :, 0] = (1 - mask) * img[:, :, 0] + mask * (overlay_color[0]*transparency + (1-transparency)*img[:, :, 0])
-    im_over[:, :, 1] = (1 - mask) * img[:, :, 1] + mask * (overlay_color[1]*transparency + (1-transparency)*img[:, :, 1])
-    im_over[:, :, 2] = (1 - mask) * img[:, :, 2] + mask * (overlay_color[2]*transparency + (1-transparency)*img[:, :, 2])
-    plt.imshow(im_over.astype(np.uint8))
-    plt.axis('off')
-    plt.show()
-    plt.pause(0.01)
-    plt.clf()
+    im_over[:, :, 0] = (1 - mask) * img[:, :, 0] + mask * (
+    overlay_color[0] * transparency + (1 - transparency) * img[:, :, 0])
+    im_over[:, :, 1] = (1 - mask) * img[:, :, 1] + mask * (
+    overlay_color[1] * transparency + (1 - transparency) * img[:, :, 1])
+    im_over[:, :, 2] = (1 - mask) * img[:, :, 2] + mask * (
+    overlay_color[2] * transparency + (1 - transparency) * img[:, :, 2])
+
+    imsave(os.path.join(result_path, 'images', img_p), im_over)
+
+    # plt.imshow(im_over.astype(np.uint8))
+    # plt.axis('off')
+    # plt.show()
+    # plt.pause(0.01)
+    # plt.clf()
